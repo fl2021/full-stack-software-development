2561a10378883bd6f73e29411a9b8e13ae1ac185
fanlu <fanlu913@gmail.com>
2021-03-28 22:30:55 -0400

    Intro to node project

---
+++ intro-to-node/index.js
@@ -0,0 +1,14 @@
+//const fs = require('fs');
+
+//fs.copyFileSync("text1.txt","text2.txt");
+
+const superheroes = require('superheroes');
+
+let all = superheroes.all;
+
+//let name = superheroes.random();
+
+console.log(all);
+
+
+
